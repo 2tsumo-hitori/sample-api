0119a639bb6a8abc893e70b55da9f5a2566aa10a
2tsumo-hitori <audghks1996@naver.com>
2023-09-08 18:23:44 +0900

    오타교정 로직

--- handler/search-movie.go
+++ handler/search-movie.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/2tsumo-hitori/sample-api/config/esclient"
 	"github.com/2tsumo-hitori/sample-api/model"
 	"github.com/2tsumo-hitori/sample-api/util"
@@ -23,6 +24,10 @@ const (
 )
 
 func SearchByKeyword[T model.Response](searchKeyword string, resp *[]T) {
+	keyword := BuildSuggestQuery(searchKeyword)
+
+	fmt.Println(keyword)
+
 	q := util.Queue{}
 
 	_, s := util.InspectSpell(searchKeyword)
@@ -37,15 +42,16 @@ func SearchByKeyword[T model.Response](searchKeyword string, resp *[]T) {
 func AutoCompleteByKeyword[T model.Response](searchKeyword string, resp *[]T) {
 	q := util.Queue{}
 
-	q.Enqueue(queryBuilderByKeyword(searchKeyword))
+	q.Enqueue(queryBuildByKeyword(searchKeyword))
 
 	sendRequestToElastic(q, resp)
 }
 
-// 검색어를 일반검색, 한/영 오타변환, 영/한 오타변환 쿼리들로 만들어 queue로 쌓고 재귀적으로 오타교정 구현
+// 검색어를 [일반검색, 한/영 오타변환, 영/한 오타변환] 쿼리들로 만들어 queue에 쌓고 재귀적으로 오타교정 구현
 // ** 오타교정 쿼리 추가예정 **
-func sendRequestToElastic[T model.Response](query util.Queue, resp *[]T) {
-	if query.IsEmpty() {
+func sendRequestToElastic[T model.Response](queryQueue util.Queue, resp *[]T) {
+	// 종료 조건
+	if queryQueue.IsEmpty() {
 		return
 	}
 
@@ -54,7 +60,7 @@ func sendRequestToElastic[T model.Response](query util.Queue, resp *[]T) {
 	searchResult, err := client.
 		Search().
 		Index(index).
-		Query(query.Dequeue().(elastic.Query)).
+		Query(queryQueue.Dequeue().(elastic.Query)).
 		Pretty(true).
 		Do(context.Background())
 
@@ -62,8 +68,9 @@ func sendRequestToElastic[T model.Response](query util.Queue, resp *[]T) {
 		panic(err)
 	}
 
+	// 결과가 없을 경우 재귀
 	if searchResult.TotalHits() == 0 {
-		sendRequestToElastic(query, resp)
+		sendRequestToElastic(queryQueue, resp)
 	}
 
 	for _, hit := range searchResult.Hits.Hits {
@@ -77,7 +84,7 @@ func sendRequestToElastic[T model.Response](query util.Queue, resp *[]T) {
 	}
 }
 
-func queryBuilderByKeyword(searchKeyword string) elastic.Query {
+func queryBuildByKeyword(searchKeyword string) elastic.Query {
 	var query elastic.Query
 
 	if spell, s := util.InspectSpell(searchKeyword); spell == word {
@@ -95,4 +102,36 @@ func queryBuilderByKeyword(searchKeyword string) elastic.Query {
 	}
 
 	return query
+}
+
+func BuildSuggestQuery(searchKeyword string) string {
+	termSuggester := elastic.NewTermSuggester("movie-suggestion").
+		Field("movieNm_text.spell").
+		StringDistance("jaro_winkler").
+		Text(searchKeyword)
+
+	query := elastic.NewSearchSource().
+		Suggester(termSuggester).
+		FetchSource(false).
+		TrackScores(true).
+		Timeout("100ms")
+
+	client := esclient.Client()
+
+	searchResult, _ := client.Search().
+		Index(index).
+		SearchSource(query).
+		Do(context.Background())
+
+	suggestions := searchResult.Suggest["movie-suggestion"]
+
+	var resp string
+
+	for _, i := range suggestions {
+		for _, j := range i.Options {
+			resp = j.Text
+		}
+	}
+
+	return resp
 }
---
+++ handler_test/search-movie_test.go
@@ -0,0 +1,15 @@
+package handler_test
+
+//func TestBuildSuggestQuery(t *testing.T) {
+//	query := handler.BuildSuggestQuery("얀녕하세요")
+//
+//	source, err := query.Source()
+//
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	sourceQuery, _ := json.Marshal(source)
+//
+//	fmt.Println(string(sourceQuery))
+//}
